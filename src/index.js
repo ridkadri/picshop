3918a10e82c2c80768a00624310af370cdc8d5d8
Ridwan Kadri <kadriridwan0@gmail.com>
2021-02-22 01:49:08 +0000

    added context and fetched images to display

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom'
+import {ContextProvider} from './Context'
 
 ReactDOM.render(
-  <Router>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </Router>,
+  <ContextProvider>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  </ContextProvider>,
   document.getElementById('root')
 );
 
