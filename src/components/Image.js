ef9c7c0abd542924f1dd9b2bd7b28fce6afc7da2
Ridwan Kadri <kadriridwan0@gmail.com>
2021-02-22 17:33:32 +0000

    propTypes, add and remove images to cart
    added and implemented propTypes on the image component, added logic to add and remove items to the cart array which changes icons on each image

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,10 @@
 import React,{useState, useContext} from "react"
+import PropTypes from 'prop-types'
 import {Context} from '../Context'
 
 function Image({className, img}) {
     const [hovered, setHovered] = useState(false)
-    const {toggleFavorite} = useContext(Context)
+    const {toggleFavorite, addToCart, removeFromCart, cartItems} = useContext(Context)
 
     function heartIcon() {
         if (img.isFavorite) {
@@ -12,10 +13,17 @@ function Image({className, img}) {
             return <i className="ri-heart-line favorite" onClick={()=> toggleFavorite(img.id)}></i>  
         }
     }
-        
-        
-    const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
+    
+    function cartIcon() {
+        const alreadyInCart = cartItems.some(item => item.id === img.id)
+        if (alreadyInCart) {
+            return <i className="ri-shopping-cart-fill cart" onClick={()=> removeFromCart(img.id)}></i>
+        } else if(hovered) {
+           return <i className="ri-add-circle-line cart" onClick={()=> addToCart(img)}></i>
 
+        }
+    }
+        
     return (
         <div 
             className={`${className} image-container`}
@@ -24,9 +32,18 @@ function Image({className, img}) {
         >
            <img src={img.url} className="image-grid"/>
             {heartIcon()}  
-            {cartIcon}
+            {cartIcon()}
         </div>
     )
+}
+
+Image.propTypes = {
+    className: PropTypes.string,
+    img: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        url: PropTypes.string.isRequired,
+        isFavorite: PropTypes.bool
+    })
 }
 
 export default Image
